61ae530ecd497b5d8b44e6f6fa1eda248f1022c0
Reza Tahmasebi <developerman.it@gmail.com>
2021-01-25 17:16:06 +0330

    Add Action To admin Panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from .models import *
+
+
+#My Actions
+def make_published(modeladmin, request, queryset):
+    published=queryset.update(status='p')
+    if(published==1):
+        msg='منتشر شد'
+    else:
+        msg='منتشر شدند'
+    modeladmin.message_user(request,"{}  مقاله  {}".format(published,msg))
+make_published.short_description = "منتشر کردن"
+
+def make_darft(modeladmin, request, queryset):
+    draft=queryset.update(status='d')
+    if(draft==1):
+        msg='پیش نویس شد'
+    else:
+        msg='پیش نویس شدند'
+    modeladmin.message_user(request,"{} مقاله {}".format(draft,msg))
+make_darft.short_description = "پیش نویس"
+
+
+
+
+
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = (
         'title',
@@ -22,6 +48,7 @@ class ArticleAdmin(admin.ModelAdmin):
     )
     prepopulated_fields = {'slug':('title',)}
     ordering = ['status']#  . be manaye nozoli
+    actions = [make_published,make_darft]
 
     def category_to_string(self,obj):
         return ", ".join([category.title for category in obj.category_published()])
